bac60ff5ffd7cdb84475150e10fc4476e2c78644
dylanbannon <dylanbannon5@gmail.com>
2019-03-03 13:52:49 -0800

    converted functionality to class and added benchmarking uploads

--- bucket-monitor.py
+++ bucket-monitor.py
@@ -14,46 +14,53 @@ from google.cloud import storage
 from redis import StrictRedis
 from redis.exceptions import ConnectionError
 
-def main():
+class BucketMonitor():
     '''
     Watch a cloud bucket and write an entry to Redis for each upload.
     '''
-    # read in environment variables
-    CLOUD_PROVIDER = os.environ['CLOUD_PROVIDER']
-    BUCKET_NAME = os.environ['BUCKET']
-    REDIS_HOST = os.environ['REDIS_HOST']
-    REDIS_PORT = os.environ['REDIS_PORT']
-    INTERVAL = os.environ['INTERVAL']
+    def __init__():
+        # read in environment variables
+        self.CLOUD_PROVIDER = os.environ['CLOUD_PROVIDER']
+        self.BUCKET_NAME = os.environ['BUCKET']
+        self.REDIS_HOST = os.environ['REDIS_HOST']
+        self.REDIS_PORT = os.environ['REDIS_PORT']
+        self.INTERVAL = os.environ['INTERVAL']
 
-    # confiugre logger
-    bm_logger = logging.getLogger('bucket-monitor')
-    bm_logger.setLevel(logging.DEBUG)
-    fh = logging.FileHandler('bucket-monitor.log')
-    fh.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
-    bm_logger.addHandler(fh)
+        # confiugre logger
+        self.bm_logger = logging.getLogger('bucket-monitor')
+        self.bm_logger.setLevel(logging.DEBUG)
+        fh = logging.FileHandler('bucket-monitor.log')
+        fh.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(
+                '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        self.bm_logger.addHandler(fh)
 
-    # establish cloud connection
-    if CLOUD_PROVIDER=="gke":
-        client = storage.Client()
-        bucket = client.get_bucket(BUCKET_NAME)
+        # establish cloud connection
+        if self.CLOUD_PROVIDER=="gke":
+            client = storage.Client()
+            bucket = client.get_bucket(self.BUCKET_NAME)
 
-    # establish Redis connection
-    r = StrictRedis(
-        host=REDIS_HOST,
-        port=REDIS_PORT,
-        decode_responses=True,
-        charset='utf-8')
+        # establish Redis connection
+        self.r = StrictRedis(
+            host=self.REDIS_HOST,
+            port=self.REDIS_PORT,
+            decode_responses=True,
+            charset='utf-8')
 
-    # get initial timestamp to act as a baseline, assuming UTC for everything
-    initial_timestamp = datetime.datetime.now(tz=pytz.UTC)
+        # get initial timestamp to act as a baseline, assuming UTC for everything
+        self.initial_timestamp = datetime.datetime.now(tz=pytz.UTC)
 
-    # enter main loop
-    while True:
+    def monitor_bucket():
+        # scan for new bucket uploads, write corresponding redis entires, and
+        # then sleep
+        while True:
+            self.scan_bucket_for_new_uploads()
+            time.sleep( int(self.INTERVAL) )
+
+    def scan_bucket_for_new_uploads():
         # logging loop beginning
-        bm_logger.debug("New loop at " + str(initial_timestamp))
+        self.bm_logger.debug("New loop at " + str(self.initial_timestamp))
         
         # get a timestamp to mark the baseline for the next loop iteration
         soon_to_be_baseline_timestamp = datetime.datetime.now(tz=pytz.UTC)
@@ -69,7 +76,7 @@ def main():
         try:
             earliest_upload = min(upload_times)
         except ValueError:
-            bm_logger.error("There may not be any folder with the " +
+            self.bm_logger.error("There may not be any folder with the " +
                     "chosen prefix. (By default, \"uploads\".)")
         uploads_length = len(all_uploads)
         upload_times_length = len(upload_times)
@@ -82,75 +89,111 @@ def main():
 
         # keep only the files whose timestamps lie between now and the last
         # iteration of this loop
-        new_uploads = []
+        self.new_uploads = []
         for upload in all_uploads:
-            if upload.updated > initial_timestamp:
-                new_uploads.append(upload)
-
-        # for each of these new files, parse necessary information from the
+            if upload.updated > self.initial_timestamp:
+                self.new_uploads.append(upload)
+        
+        # for each of these new uploads, parse necessary information from the
         # filename and write an appropriate entry to Redis
         # Before writing, also check that no hash already in Redis contains
         # this file's name. (We seem to have a problem with double
         # entries in Redis.)
+        self._get_all_redis_keys()
+        self._write_new_redis_keys()
+
+        # update baseline timestamp
+        self.initial_timestamp = soon_to_be_baseline_timestamp
+
+    def _get_all_redis_keys():
         while True:
             try:
-                all_keys = r.keys()
+                all_keys = self.r.keys()
                 break
             except ConnectionError:
                 # For some reason, we're unable to connect to Redis right now.
                 # Keep trying until we can.
-                bm_logger.warn("Trouble connecting to Redis. Retrying.")
+                self.bm_logger.warn("Trouble connecting to Redis. Retrying.")
                 time.sleep(5)
-        combined_keys = '\t'.join(all_keys) # long string containing all keys
-        for upload in new_uploads:
+        self.combined_keys = '\t'.join(all_keys) # long string containing all keys
+
+    def _write_new_redis_keys():
+        for upload in self.new_uploads:
+            # verify that we're dealing with a direct upload, and not a web
+            # upload
             re_filename = '(uploads(?:/|%2F))(directupload_.+)$'
             try:
                 upload_filename = re.search(re_filename, upload.path).group(2)
             except AttributeError:
                 # this isn't a directly uploaded file
                 # or its filename was formatted incorrectly
-                bm_logger.debug("Failed on filename of " +
+                self.bm_logger.debug("Failed on filename of " +
                         str(upload.path) + ".")
                 continue
+
             # check for presence of filename in Redis already
-            if upload_filename in combined_keys:
-                bm_logger.debug(upload_filename +
+            if upload_filename in self.combined_keys:
+                self.bm_logger.debug(upload_filename +
                         " tried to get uploaded a second time.")
                 continue
-            # dictionary for uploading to Redis
-            field_dict = {}
-            field_dict['url'] = upload.public_url
-            field_dict['file_name'] = "uploads/" + upload_filename
-            field_dict['status'] = "new"
-            # filename schema: modelname_modelversion_ppfunc_cuts_etc
-            re_fields = 'directupload_([^_]+)_([0-9]+)_([^_]+)_([0-9]+)_.+$'
-            fields = re.search(re_fields, upload_filename)
-            try:
-                field_dict['model_name'] = fields.group(1)
-                field_dict['model_version'] = fields.group(2)
-                field_dict['postprocessing_function'] = fields.group(3)
-                field_dict['cuts'] = fields.group(4)
-            except AttributeError:
-                bm_logger.debug("Failed on fields of " +
-                        str(upload_filename) + ".")
-                continue
-            field_dict['timestamp_upload'] = time.time() * 1000
-            redis_key = "predict_" + uuid.uuid4().hex + \
-                    "_" + upload_filename
-            while True:
-                try:
-                    r.hmset(redis_key, field_dict)
-                    break
-                except ConnectionError:
-                    # For some reason, we're unable to connect to Redis
-                    # right now. Keep trying until we can.
-                    bm_logger.warn("Trouble connecting to Redis. Retrying.")
-                    time.sleep(5)
-            bm_logger.debug("Wrote Redis entry for " + upload_filename + ".")
 
-        # update baseline timestamp and sleep
-        initial_timestamp = soon_to_be_baseline_timestamp
-        time.sleep( int(INTERVAL) )
+            # check to see whether this is a special "benchmarking" direct
+            # upload
+            benchmarking_re = 'benchmarking([0-9]+)special'
+            benchmarking_result = re.search(benchmarking_re, upload_filename)
+            if benchmarking_result is None:
+                # standard direct upload
+                self._create_single_redis_entry(upload, upload_filename)
+            else:
+                # "benchmarking" direct upload
+                number_of_images = benchmarking_result.group(1)
+                self._create_multiple_redis_entries(upload, upload_filename,
+                        number_of_images)
+
+    def _create_multiple_redis_entries(self, upload, upload_filename, 
+            number_of_images):
+        # make a numbe rof redis entries corresponding to the number found in
+        # the benchmarking section of the filename
+        for img_num in range(number_of_images):
+            upload_filename = upload_filename + str(img_num)
+            _create_single_redis_entry(upload, upload_filename)
+
+    def _create_single_redis_entry(self, upload, upload_filename):
+        # dictionary for uploading to Redis
+        field_dict = {}
+        field_dict['url'] = upload.public_url
+        field_dict['file_name'] = "uploads/" + upload_filename
+        field_dict['status'] = "new"
+        # filename schema: modelname_modelversion_ppfunc_cuts_etc
+        re_fields = 'directupload_([^_]+)_([0-9]+)_([^_]+)_([0-9]+)_.+$'
+        fields = re.search(re_fields, upload_filename)
+        try:
+            field_dict['model_name'] = fields.group(1)
+            field_dict['model_version'] = fields.group(2)
+            field_dict['postprocessing_function'] = fields.group(3)
+            field_dict['cuts'] = fields.group(4)
+        except AttributeError:
+            self.bm_logger.debug("Failed on fields of " +
+                    str(upload_filename) + ".")
+            return 0
+
+        field_dict['timestamp_upload'] = time.time() * 1000
+        redis_key = "predict_" + uuid.uuid4().hex + \
+                "_" + upload_filename
+        self._write_redis_entry(redis_key, field_dict)
+
+    def _write_redis_entry(self, redis_key, field_dict):
+        while True:
+            try:
+                r.hmset(redis_key, field_dict)
+                break
+            except ConnectionError:
+                # For some reason, we're unable to connect to Redis
+                # right now. Keep trying until we can.
+                self.bm_logger.warn("Trouble connecting to Redis. Retrying.")
+                time.sleep(5)
+        self.bm_logger.debug("Wrote Redis entry for " + upload_filename + ".")
 
 if __name__=='__main__':
-    main()
+    bm = BucketMonitor()
+    bm.monitor_bucket()
